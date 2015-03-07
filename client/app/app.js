7a119e297e57734fe124230478d7676968d6064c
lpww <t.treffry@gmail.com>
2015-03-06 19:44:32 -0800

    Add: Live search Add: sort according to visits count

--- client/app/app.js
+++ client/app/app.js
@@ -7,10 +7,6 @@
 ])
 .config(function($routeProvider, $httpProvider) {
   $routeProvider
-    .when('/', {
-      templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
-    })
     .when('/signin', {
       templateUrl: 'app/auth/signin.html',
       controller: 'AuthController'
@@ -26,6 +22,10 @@
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
+    })
+    .otherwise({
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
     });
 
     // We add our $httpInterceptor into the array
--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,9 +2,13 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
+  $scope.searchLink = '';
 
   $scope.getLinks = function(){
     Links.getLinks().then(function(result){
+      result.sort(function(a,b){
+        return b.visits - a.visits;
+      });
       $scope.data.links = result;
     });
   };
