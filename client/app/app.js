ebb8cd769ce46b8cc8908dd81b1d8c05d638cd0b
lpww <t.treffry@gmail.com>
2015-03-07 11:39:58 -0800

    Add: form validation

--- client/app/app.js
+++ client/app/app.js
@@ -9,23 +9,28 @@
   $routeProvider
     .when('/signin', {
       templateUrl: 'app/auth/signin.html',
-      controller: 'AuthController'
+      controller: 'AuthController',
+      authenticate: false
     })
     .when('/signup', {
       templateUrl: 'app/auth/signup.html',
-      controller: 'AuthController'
+      controller: 'AuthController',
+      authenticate: false
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     })
     .otherwise({
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     });
 
     // We add our $httpInterceptor into the array
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,16 +1,19 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  $scope.link = {
-  };
-
+  $scope.link = '';
   $scope.recent = [];
+  $scope.regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+  $scope.errorMessage = 'Please enter a valid link';
 
-  $scope.addLink = function(){
-    console.log(Links);
-    Links.addLink($scope.link).then(function(res){
-      $scope.recent.push(res);
-    });
+  $scope.addLink = function(isValid){
+    if(isValid){
+      Links.addLink($scope.link).then(function(res){
+        $scope.recent.push(res);
+      });
+      this.link = '';
+    } else {
+      $scope.errorMessage = 'you  are abadd';
+    }
   };
-
 });
